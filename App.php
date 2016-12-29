609ab1292db8a68e4513cd30fc3887647e38f42a
Subin Siby <subins2000@gmail.com>
2016-12-29 16:09:00 +0530

    Version 0.3.1

--- App.php
+++ App.php
@@ -2,15 +2,15 @@
 namespace Lobby\App;
 
 class ymp3 extends \Lobby\App{
-  
+
   public function page($page){
     return "auto";
   }
-  
+
   public function indexPage(){
     $this->addStyle("cdn/main.css");
     $this->addScript("cdn/main.js");
-    
+
     return array($this->dir . "/page-index.php");
   }
 }
